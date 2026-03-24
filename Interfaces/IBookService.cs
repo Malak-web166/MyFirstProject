50c86d6b191db9912e0fd6716bd9a1e0632efc73
Malak-web166 <your-github-email@example.com>
2026-03-24 03:38:18 +0200

    Updated my controllers and services

--- Interfaces/IBookService.cs
+++ Interfaces/IBookService.cs
@@ -2,9 +2,8 @@ using MyAssignmentProject.Models;
 
 namespace MyAssignmentProject.Interfaces;
 
-public interface IBookService
-{
-    IEnumerable<Book> GetAll();      // Endpoint 1: List all books
-    Book GetById(int id);            // Endpoint 2: Get one book by ID
-    void Add(Book book);             // Endpoint 3: Create a new book
+public interface IBookService {
+    IEnumerable<Book> GetAll();      // Endpoint 1: List all
+    Book GetById(int id);            // Endpoint 2: Get one
+    void Create(Book book);          // Endpoint 3: Add new
 }
